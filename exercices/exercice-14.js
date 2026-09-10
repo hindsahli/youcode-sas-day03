b701a877204f31cb40147d0b82633b726c330857
hindsahli <hindsahlii.2003@gmail.com>
2026-09-10 21:17:49 +0200

    jour 03 : terminer le niveau 1

---
+++ exercices/exercice-14.js
@@ -0,0 +1,50 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 03 · EXERCICE 14 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * CALCULATRICE MODULAIRE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Écrivez une fonction calculatrice(a, b, signe). Utilisez un switch sur signe pour appeler l'une des 4 opérations de base 
+ * (+, -, *, /) que vous aurez écrites en tant que fonctions distinctes.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-14
+ * ▶️ Commande : node day03/exercices/exercice-14.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+// TODO: écris ta solution ici.
+
+function  addition(a, b)
+{
+    return (a+b);
+}
+function soustraction(a, b)
+{
+    return (a-b);
+}
+function multiplication(a, b)
+{
+    return (a*b);
+}
+function Division(a, b)
+{
+    return (a/b);
+}
+function calculatrice(a, b, signe)
+{
+    switch (signe) {
+        case "+":
+            return addition(a, b)
+        case "-":
+            return soustraction(a, b)
+        case "*":
+            return multiplication(a, b)
+        case "/":
+            return Division(a, b)
+    }
+}
+
+console.log(calculatrice(6,3,"/"))
