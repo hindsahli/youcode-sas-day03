12a29fe94810fdb1a1090273f0dc5d65cba80e46
hindsahli <hindsahlii.2003@gmail.com>
2026-09-11 13:25:50 +0200

    jour 03 : terminer le niveau 1

--- exercices/exercice-17.js
+++ exercices/exercice-17.js
@@ -16,3 +16,18 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+function extrairePrixEtConvertir(chaineBrute)
+{
+    let m = ""
+    let i = 0
+    while(i < chaineBrute.length)
+    {
+        if(/[0-9]/.test(chaineBrute[i]) == true)
+            m += chaineBrute[i]
+        i++
+    }
+    return m
+}
+console.log(extrairePrixEtConvertir("   Prix: 15.99 $  "))
+
